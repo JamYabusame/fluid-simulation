3b88f80869ad81360998bf5df1737d6269ae008d
root <root@JamYabusame20041229.localdomain>
2026-06-14 21:26:20 +0900

    edit the material of the box

--- src/index.js
+++ src/index.js
@@ -14,25 +14,30 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth/ window.innerHeight, 1, 2000);
-const light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+const light = new THREE.DirectionalLight(0xFFFFFF, 2.0);
 light.position.set(1, 1, 1);
 camera.position.set(2,2,2);
 
+//Make a box
 const box1_geo = new THREE.BoxGeometry(1,0.1,1);
-const box_mat = new THREE.MeshPhongMaterial({color: 0xFFFFFF});
+const box_mat = new THREE.MeshPhongMaterial({transparent: true, opacity: 0.5,});
 const box1_mesh = new THREE.Mesh(box1_geo, box_mat);
 const box2_geo = new THREE.BoxGeometry(1,0.5,0.1);
-const box3_geo = new THREE.BoxGeometry(0.1,0.5,1.0);
+const box3_geo = new THREE.BoxGeometry(0.1,1.0,1.0);
+const box4_geo = new THREE.BoxGeometry(0.1,0.5,1.0);
 const box2_mesh = new THREE.Mesh(box2_geo, box_mat);
 const box3_mesh = new THREE.Mesh(box2_geo, box_mat);
 const box4_mesh = new THREE.Mesh(box3_geo, box_mat);
+const box5_mesh = new THREE.Mesh(box4_geo, box_mat);
 box2_mesh.position.set(0,0.25,0.55);
 box3_mesh.position.set(0,0.25,-0.55);
-box4_mesh.position.set(0.55,0.25,0);
+box4_mesh.position.set(0.55,0.5,0);
+box5_mesh.position.set(-0.55,0.25,0);
 scene.add(box1_mesh);
 scene.add(box2_mesh);
 scene.add(box3_mesh);
 scene.add(box4_mesh);
+scene.add(box5_mesh);
 
 
 camera.lookAt(new THREE.Vector3(0,0,0));
